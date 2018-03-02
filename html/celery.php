b94fae3e2c9a35bfa8373251e4d78ff17db71fd9
Berm MBA11 <berm@mba11.mac>
2018-03-02 21:53:30 +0700

    docker: apache php

---
+++ html/celery.php
@@ -0,0 +1,29 @@
+<?php
+
+class Celery {
+
+    /**
+      * Run celery task
+      *
+      * @return uuid
+      */
+    function runTask($action) {
+        $uuid = 'uuid';
+
+        return $uuid;
+    }
+
+    /**
+      * Check output of task
+      * @param uuid
+      * 
+      * @return result
+      */
+    function checkResult($uuid) {
+        $result = 'result';
+
+        return $result;
+    }
+}
+
+?>
